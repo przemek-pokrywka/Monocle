19ac385d9289d981300387c541c986bff27fecf7
Julien Truffaut <truffaut.julien@gmail.com>
2020-01-06 10:24:22 +0000

    update examples

--- core/shared/src/test/scala-2.13/monocle/TypeInferenceTest.scala
+++ core/shared/src/test/scala-2.13/monocle/TypeInferenceTest.scala
@@ -10,7 +10,6 @@ class TypeInferenceTest extends AnyFunSuite with Matchers {
   test("monomorphic") {
     def id[A]: Iso[Id[A], A] = Iso[Id[A], A](_.value)(Id(_))
 
-    // works
     Prism.some.getOption(Some("foo")) shouldEqual Some("foo")
     Prism.some.set("bar")(Some("foo")) shouldEqual Some("bar")
 
@@ -26,6 +25,7 @@ class TypeInferenceTest extends AnyFunSuite with Matchers {
     // [error]  required: monocle.Prism[Some[Id[Int]],?]
 //    Some(Id(5)).optic.composePrism(Prism.some).composeIso(id).getOption shouldEqual Some(5)
 
+    // here is an example where variance in optics would help
     Option(Id(5)).optic.composePrism(Prism.some).composeIso(id).getOption shouldEqual Some(5)
     Option(Id(5)).optic.composePrism(Prism.some).composeIso(id).set(3) shouldEqual Option(Id(3))
 
@@ -34,7 +34,6 @@ class TypeInferenceTest extends AnyFunSuite with Matchers {
   test("polymorphic") {
     def id[A, B]: PIso[Id[A], Id[B], A, B] = PIso[Id[A], Id[B], A, B](_.value)(Id(_))
 
-    // works
     PPrism.some.getOption(Some("foo")) shouldEqual Some("foo")
     PPrism.some.set(5)(Some("foo")) shouldEqual Some(5)
 
@@ -43,6 +42,8 @@ class TypeInferenceTest extends AnyFunSuite with Matchers {
     //[error]  required: monocle.poly.PIso[Id[Nothing],Id[Nothing],Nothing,C2]
 //    PPrism.some.andThenIso(id).getOption(Some(Id(5))) shouldEqual Some(5)
 
+    PPrism.some[Id[Int], Id[String]].andThenIso(id).set("foo")(Some(Id(5))) shouldEqual Some(Id("foo"))
+
     import monocle.poly.applied.syntax._
 
     // type mismatch;
@@ -50,12 +51,13 @@ class TypeInferenceTest extends AnyFunSuite with Matchers {
     // [error]  required: monocle.poly.PPrism[Some[Id[Int]],Option[Nothing],Id[Int],C2]
 //    Some(Id(5)).optic.andThenPrism(poly.PPrism.some).andThenIso(id).getOption shouldEqual Some(5)
 
-    Some(Id(5)).optic.andThenPrism(poly.Prism.some).andThenIso(id).getOption shouldEqual Some(5)
+    // type mismatch;
+    //[error]  found   : monocle.poly.PPrism[Option[Id[Int]],Option[Nothing],Id[Int],Nothing]
+    //[error]  required: monocle.poly.PPrism[Some[Id[Int]],Option[Id[String]],Id[Int],C2]
+//    Some(Id(5)).optic[Option[Id[String]]].andThenPrism(poly.PPrism.some).andThenIso(id).set("foo") shouldEqual Some(Id("foo"))
 
-    // polymorphic expression cannot be instantiated to expected type;
-    //[error]  found   : [A, B]monocle.poly.PPrism[Option[A],Option[B],A,B]
-    //[error]  required: monocle.poly.PPrism[Some[Id[Int]],String,?,?]
-//    Some(Id(5)).optic[String].andThenPrism(poly.PPrism.some).andThenIso(id).set("foo") shouldEqual Some("foo")
+    Some(Id(5)).optic.andThenPrism(poly.Prism.some).andThenIso(id).getOption shouldEqual Some(5)
+    Some(Id(5)).optic.andThenPrism(poly.Prism.some).andThenIso(id).set(4) shouldEqual Some(Id(4))
 
   }
 
